cd556eb5c14dad98c310ec207465deefd4e6d69d
edsonplz <edson.silva@dcx.ufpb.br>
2025-05-21 09:15:28 -0300

    Seção monitores e slider de patrocinadores adicionado!

--- script.js
+++ script.js
@@ -51,3 +51,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
     })
 })
+
+// Slider de Patrocinadores
+
+window.addEventListener('DOMContentLoaded', () => {
+    const slider = document.querySelector('.slider-sponsors');
+    const images = slider.querySelectorAll('img');
+
+    images.forEach(image=> {
+        const clone = image.cloneNode(true);
+        slider.appendChild(clone);
+    });
+
+    const totalWidth = images.length * (images[0].offsetWidth + 20);
+
+    slider.style.width = `${totalWidth}px`;
+
+    let currentPosition = 0;
+
+    const moveSlider = () => {
+        currentPosition -= 1;
+        if(currentPosition <= -totalWidth/2){
+            currentPosition = 0;
+        }
+
+        slider.style.transform = `translateX(${currentPosition}px)`;
+        requestAnimationFrame(moveSlider);
+    }
+
+    requestAnimationFrame(moveSlider);
+})
